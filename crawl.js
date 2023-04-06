2abc1f3f75b615d1852ef0480619691e3ae6ff90
ramses <cevallosramses@gmail.com>
2023-04-06 17:14:11 -0600

    crawl page recursively working

--- crawl.js
+++ crawl.js
@@ -1,29 +1,55 @@
 const { JSDOM } = require('jsdom')
 
-async function crawlPage(currentURL) {
+async function crawlPage(baseURL, currentURL, pages) {
+    // Verify that the current url is in the same domain as the base url
+    const baseURLObj = new URL(baseURL)
+    const currentURLObj = new URL(currentURL)
+    if (baseURLObj.hostname !== currentURLObj.hostname) {
+        return pages
+    }
+
+    // Get normalize version of the current url
+    const normalizeCurrentURL = normalizeURL(currentURL)
+    // Check if the current page has been crawled, and increment count if so
+    if (pages[normalizeCurrentURL] > 0) {
+        pages[normalizeCurrentURL]++
+        return pages
+    }
+    // Else start the count for current normalize url
+    pages[normalizeCurrentURL] = 1
     console.log(`actively crawling ${currentURL}`)
 
+    // Fetch the page to get html body 
     try {
         const resp = await fetch(currentURL)
 
         if (resp.status > 399) {
             console.log(`error in fech with status code: ${resp.status}, on page ${currentURL}`)
-            return
+            return pages
         }
 
         const contentType = resp.headers.get('content-type')
         if (!contentType.includes('text/html')) {
             console.log(`non html response, content type: ${contentType}, on page ${currentURL}`)
-            return
+            return pages
         }
 
-        console.log(await resp.text())
+        const htmlBody = await resp.text()
+        const nextURLs = getURLsFromHTML(htmlBody, baseURL)
+
+        // Recursivly crawl pages within the main page
+        for (let nextURL of nextURLs) {
+            pages = await crawlPage(baseURL, nextURL, pages)
+        }
+
     } catch (err) {
         console.log(`error in fetch: ${err.message}, on page: ${currentURL}`)
     }
+    // When done crawling all pages, return the pages object
+    return pages
 }
 
-// Takes html body and retrieves the href attribute from the a tags
+// Takes html body and retrieves urls from the href attribute from the a tags
 function getURLsFromHTML(htmlBody, baseURL) {
     const urls = []
     const dom = new JSDOM(htmlBody)
@@ -59,16 +85,16 @@ function getURLsFromHTML(htmlBody, baseURL) {
 // and normalize the output:
 // 'https://clinicavitalcare.com' -> clinicavitalcare.com
 // 'http://clinicavitalcare.com' -> clinicavitalcare.com
-// 'www.clinicavitalcare.com' -> clinicavitalcare.com
+// 'www.clinicavitalcare.com/' -> clinicavitalcare.com
 function normalizeURL(urlString) {
     const urlObj = new URL(urlString)
-    const hostName = `${urlObj.hostname}`
+    const fullPath = `${urlObj.hostname}${urlObj.pathname}`
 
     // If the hostname ends with '/' removes it
-    if (hostName.length > 0 && hostName.slice(-1) === '/') {
-        return hostName.slice(0, -1)
+    if (fullPath.length > 0 && fullPath.slice(-1) === '/') {
+        return fullPath.slice(0, -1)
     }
-    return hostName
+    return fullPath
 }
 
 module.exports = {
--- main.js
+++ main.js
@@ -1,16 +1,22 @@
 const { crawlPage } = require('./crawl')
 
-function main () {
+async function main () {
     if (process.argv.length < 3) {
         console.log('no website provided')
         process.exit(1)
-    } else if (process.argv.length > 3) {
+    }
+    if (process.argv.length > 3) {
         console.log('too many command line args')
         process.exit(1)
     }
+
     const baseURL = process.argv[2]
     console.log(`starting crawl of ${baseURL}`)
-    crawlPage(baseURL)
+    const pages = await crawlPage(baseURL, baseURL, {})
+    console.log(pages)
+    for (const page of Object.entries(pages)) {
+        console.log(page)
+    }
 }
 
 main()
